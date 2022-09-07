eb4d6c2f2e519c46c4768de0461743a0668857a9
Olayode Enoch <olayodeenoch@gmail.com>
2022-09-07 16:48:22 +0100

    Added dynamic footer and header to pages

---
+++ apart/footer.php
@@ -0,0 +1,80 @@
+<section class="footer-2">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-sm-6 col-md-8 col-xl-3 col-sm-6">
+                <div class="widget footer-about mb-5 mb-lg-0">
+                    <h5 class="widget-title text-gray">About us</h5>
+                    <ul class="list-unstyled footer-info">
+                        <li><span>Ph:</span>+(68) 345 5902</li>
+                        <li><span>Email:</span>info@yourdomain.com</li>
+                        <li><span>Location:</span> 123 Fifth Floor East 26th Street, New York, NY 10011</li>
+                    </ul>
+                    <ul class="list-inline footer-socials">
+                        <li class="list-inline-item">Follow us :</li>
+                        <li class="list-inline-item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                        <li class="list-inline-item"> <a href="#"><i class="fab fa-twitter"></i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="fab fa-linkedin"></i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="fab fa-pinterest"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+
+
+            <div class="col-xl-7 ml-auto col-lg-7 col-md-12 col-sm-12">
+                <div class="row">
+                    <div class="col-lg-4 col-xl-4 col-sm-4 col-md-4 ">
+                        <div class="footer-widget mb-5 mb-lg-0">
+                            <h5 class="widget-title text-gray">Explore</h5>
+                            <ul class="list-unstyled footer-links">
+                                <li><a href="#">About us</a></li>
+                                <li><a href="#">Contact us</a></li>
+                                <li><a href="#">Services</a></li>
+                                <li><a href="#">Support</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-xl-4 col-sm-4 col-md-4">
+                        <div class="footer-widget mb-5 mb-lg-0">
+                            <h5 class="widget-title text-gray">Courses</h5>
+                            <ul class="list-unstyled footer-links">
+                                <li><a href="#">SEO Business</a></li>
+                                <li><a href="#">Digital Marketing</a></li>
+                                <li><a href="#">Graphic Design</a></li>
+                                <li><a href="#">Social Marketing</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-xl-4 col-sm-4 col-md-4">
+                        <div class="footer-widget mb-5 mb-lg-0">
+                            <h5 class="widget-title text-gray">Legal</h5>
+                            <ul class="list-unstyled footer-links">
+                                <li><a href="#">Terms & Condition</a></li>
+                                <li><a href="#">Privacy policy</a></li>
+                                <li><a href="#">Return policy</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-btm">
+        <div class="container">
+            <div class="row justify-content-center align-items-center">
+                <div class="col-xl-6 col-lg-4 col-md-12">
+                    <div class="footer-logo text-lg-left text-center mb-4 mb-lg-0">
+                        <img src="assets/images/light-logo.png" alt="EduHash" class="img-fluid">
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-8 col-md-12">
+                    <div class="copyright text-lg-right text-center">
+                        <p>© Copyright EduHash Template All rights reserved.Crafted by <a href="https://themeturn.com">Dreambuzz</a> </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
