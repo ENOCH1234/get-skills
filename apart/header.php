d140a8f6e653801f33df8bedad68aa1729461878
Olayode Enoch <olayodeenoch@gmail.com>
2022-09-07 16:40:44 +0100

    Pushed unused files to pages folder

---
+++ apart/header.php
@@ -0,0 +1,123 @@
+<header>
+    <!-- Main Menu Start -->
+    <div class="site-navigation main_menu menu-style-2" id="mainmenu-area">
+        <nav class="navbar navbar-expand-lg">
+            <div class="container">
+                <a class="navbar-brand" href="index.html">
+                    <img src="assets/images/dark-logo.png" alt="Eduhash" class="img-fluid">
+                </a>
+
+                <!-- Toggler -->
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="fa fa-bars"></span>
+                </button>
+
+                <!-- Collapse -->
+                <div class="collapse navbar-collapse" id="navbarMenu">
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbar3" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Home<i class="fa fa-angle-down"></i>
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbar3">
+                                <a class="dropdown-item " href="index.html">
+                                    Home 1
+                                </a>
+                                <a class="dropdown-item " href="index-2.html">
+                                    Home 2
+                                </a>
+                                <a class="dropdown-item " href="index-3.html">
+                                    Home 3
+                                </a>
+                                <a class="dropdown-item " href="index-4.html">
+                                    Home 4
+                                </a>
+                            </div>
+                        </li>
+                        <li class="nav-item ">
+                            <a href="about.html" class="nav-link js-scroll-trigger">
+                                About us
+                            </a>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbar3" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Courses<i class="fa fa-angle-down"></i>
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbar3">
+                                <a class="dropdown-item " href="course-grid.html">
+                                    Course Style 1
+                                </a>
+                                <a class="dropdown-item " href="course-grid-2.html">
+                                    Course Style 2
+                                </a>
+
+                                <a class="dropdown-item " href="course-grid-3.html">
+                                    Course Style 3
+                                </a>
+                                <a class="dropdown-item " href="course-grid-4.html">
+                                    Course Style 4
+                                </a>
+                                <a class="dropdown-item " href="course-single.html">
+                                    Course Details Style 1
+                                </a>
+                                <a class="dropdown-item " href="course-single2.html">
+                                    Course Details Style 2
+                                </a>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbar3" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Pages<i class="fa fa-angle-down"></i>
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbar3">
+                                <a class="dropdown-item " href="login.html">
+                                    Login
+                                </a>
+                                <a class="dropdown-item " href="register.html">
+                                    Register
+                                </a>
+                                <a class="dropdown-item " href="cart.html">
+                                    cart
+                                </a>
+                                <a class="dropdown-item " href="checkout.html">
+                                    checkout
+                                </a>
+                                <a class="dropdown-item " href="error.html">
+                                    404
+                                </a>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbar3" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Blog<i class="fa fa-angle-down"></i>
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbar3">
+                                <a class="dropdown-item " href="blog.html">
+                                    Blog
+                                </a>
+                                <a class="dropdown-item " href="blog-single.html">
+                                    Blog Single
+                                </a>
+                            </div>
+                        </li>
+
+                        <li class="nav-item ">
+                            <a href="contact.html" class="nav-link">
+                                Contact
+                            </a>
+                        </li>
+                    </ul>
+
+                    <div class="header-login">
+                        <a href="#" class="btn btn-solid-border btn-sm ">Log In</a>
+                        <a href="#" class="btn btn-main btn-sm">sign up</a>
+                    </div>
+                </div>
+                <!-- / .navbar-collapse -->
+            </div>
+            <!-- / .container -->
+        </nav>
+    </div>
+</header>
