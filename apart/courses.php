74042dbc04c2bdc653aa4a5a317b112b92008f43
Olayode Enoch <olayodeenoch@gmail.com>
2022-09-10 06:24:44 +0100

    Added new images to courses

--- apart/courses.php
+++ apart/courses.php
@@ -4,8 +4,8 @@
             <div class="col-lg-7">
                 <div class="section-heading center-heading">
                     <span class="subheading">Trending Courses</span>
-                    <h3>Over 200+ New Online Courses</h3>
-                    <p>The ultimate planning solution for busy women who want to reach their personal goals</p>
+                    <h3>Cheap Digital Courses</h3>
+                    <p>Updrage you skills and earn more today!</p>
                 </div>
             </div>
         </div>
@@ -13,127 +13,165 @@
         <div class="text-center">
             <ul class="course-filter">
                 <li class="active"><a href="#" data-filter="*"> All</a></li>
-                <li><a href="#" data-filter=".cat1">printing</a></li>
-                <li><a href="#" data-filter=".cat2">Web</a></li>
-                <li><a href="#" data-filter=".cat3">illustration</a></li>
-                <li><a href="#" data-filter=".cat4">media</a></li>
-                <li><a href="#" data-filter=".cat5">crafts</a></li>
+                <li><a href="#" data-filter=".cat1">Graphics</a></li>
+                <li><a href="#" data-filter=".cat2">Programming</a></li>
+                <li><a href="#" data-filter=".cat3">Writing</a></li>
+                <li><a href="#" data-filter=".cat4">Digital Marketing</a></li>
+                <li><a href="#" data-filter=".cat5">Trades</a></li>
             </ul>
         </div>
 
         <div class="row course-gallery ">
-            <div class="course-item cat1 cat3 col-lg-4 col-md-6">
+            <div class="course-item cat1 col-lg-4 col-md-6">
                 <div class="course-block">
                     <div class="course-img">
-                        <img src="assets/images/course/course-1.jpg" alt="" class="img-fluid">
-                        <div class="course-price2">$100</div>
+                        <img src="assets/images/course/c1.png" alt="" class="img-fluid">
+                        <!-- <div class="course-price2">$100</div> -->
                     </div>
 
                     <div class="course-content">
-                        <div class="course-meta">
+                        <!-- <div class="course-meta">
                             <span class="course-student"><i class="fa fa-user-alt"></i>340 Students</span>
                             <span class="course-duration"><i class="far fa-file-alt"></i>82 Lessons</span>
                             <span class="course-label">new</span>
-                        </div>
+                        </div> -->
 
-                        <h4><a href="#">React – The Complete Guide (React Router)</a></h4>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, alias.</p>
+                        <h4><a href="https://oke.io/6e8Ro8P" target="_blank">Smartphone Graphics Design Blueprint</a></h4>
                     </div>
                 </div>
             </div>
 
-            <div class="course-item cat2 cat4 col-lg-4 col-md-6">
-                <div class="course-block">
-                    <div class="course-img">
-                        <img src="assets/images/course/course-2.jpg" alt="" class="img-fluid">
-                        <div class="course-price2">$70</div>
-                    </div>
-
-                    <div class="course-content">
-                        <div class="course-meta">
-                            <span class="course-student"><i class="fa fa-user-alt"></i>340 Students</span>
-                            <span class="course-duration"><i class="far fa-file-alt"></i>82 Lessons</span>
-                        </div>
-
-                        <h4><a href="#">Photography Crash Course for Photographer</a></h4>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, alias.</p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="course-item cat5 cat2 col-lg-4 col-md-6">
+            <div class="course-item cat5 col-lg-4 col-md-6">
                 <div class="course-block">
                     <div class="course-img">
                         <img src="assets/images/course/course-3.jpg" alt="" class="img-fluid">
-                        <div class="course-price2">$160</div>
+                        <!-- <div class="course-price2">$70</div> -->
                     </div>
 
                     <div class="course-content">
-                        <div class="course-meta">
+                        <!-- <div class="course-meta">
                             <span class="course-student"><i class="fa fa-user-alt"></i>340 Students</span>
                             <span class="course-duration"><i class="far fa-file-alt"></i>82 Lessons</span>
-                        </div>
+                        </div> -->
 
-                        <h4><a href="#">Information About UI/UX Design Degree</a></h4>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, alias.</p>
+                        <h4><a href="https://oke.io/b04jTbv" target="_blank">Upwork Mastermind for Smartphone Users</a></h4>
+                        <!-- <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, alias.</p> -->
                     </div>
                 </div>
             </div>
 
-            <div class="course-item cat2 cat4 col-lg-4 col-md-6">
-                <div class="course-block">
-                    <div class="course-img">
-                        <img src="assets/images/course/course-2.jpg" alt="" class="img-fluid">
-                        <div class="course-price2">$130</div>
-                    </div>
-
-                    <div class="course-content">
-                        <div class="course-meta">
-                            <span class="course-student"><i class="fa fa-user-alt"></i>340 Students</span>
-                            <span class="course-duration"><i class="far fa-file-alt"></i>82 Lessons</span>
-                        </div>
-
-                        <h4><a href="#">Photography Crash Course for Photographer</a></h4>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, alias.</p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="course-item cat1 cat3 col-lg-4 col-md-6">
+            <div class="course-item cat1 col-lg-4 col-md-6">
                 <div class="course-block">
                     <div class="course-img">
                         <img src="assets/images/course/course-1.jpg" alt="" class="img-fluid">
-                        <div class="course-price2">$200</div>
+                        <!-- <div class="course-price2">$160</div> -->
                     </div>
 
                     <div class="course-content">
-                        <div class="course-meta">
+                        <!-- <div class="course-meta">
                             <span class="course-student"><i class="fa fa-user-alt"></i>340 Students</span>
                             <span class="course-duration"><i class="far fa-file-alt"></i>82 Lessons</span>
-                            <span class="course-label">new</span>
-                        </div>
+                        </div> -->
 
-                        <h4><a href="#">React – The Complete Guide (React Router)</a></h4>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, alias.</p>
+                        <h4><a href="https://oke.io/M127M" target="_blank">Graphic Design Masterclass Course</a></h4>
+                        <!-- <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, alias.</p> -->
                     </div>
                 </div>
             </div>
 
-            <div class="course-item cat5 cat2 col-lg-4 col-md-6">
+            <div class="course-item cat4 col-lg-4 col-md-6">
                 <div class="course-block">
                     <div class="course-img">
-                        <img src="assets/images/course/course-3.jpg" alt="" class="img-fluid">
-                        <div class="course-price2">$210</div>
+                        <img src="assets/images/course/course-2.jpg" alt="" class="img-fluid">
+                        <!-- <div class="course-price2">$200</div> -->
+                        <!-- DONE -->
                     </div>
 
                     <div class="course-content">
-                        <div class="course-meta">
+                        <!-- <div class="course-meta">
                             <span class="course-student"><i class="fa fa-user-alt"></i>340 Students</span>
                             <span class="course-duration"><i class="far fa-file-alt"></i>82 Lessons</span>
-                        </div>
+                            <span class="course-label">new</span>
+                        </div> -->
 
-                        <h4><a href="#">Information About UI/UX Design Degree</a></h4>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, alias.</p>
+                        <h4><a href="https://oke.io/Fa87eop" target="_blank">Make $100 - $500 Daily From CPA Marketing</a></h4>
+                        <!-- <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, alias.</p> -->
+                    </div>
+                </div>
+            </div>
+
+            <div class="course-item cat3 col-lg-4 col-md-6">
+                <div class="course-block">
+                    <div class="course-img">
+                        <img src="assets/images/course/c2.png" alt="" class="img-fluid">
+                        <!-- <div class="course-price2">$130</div> -->
+                    </div>
+
+                    <div class="course-content">
+                        <!-- <div class="course-meta">
+                            <span class="course-student"><i class="fa fa-user-alt"></i>340 Students</span>
+                            <span class="course-duration"><i class="far fa-file-alt"></i>82 Lessons</span>
+                        </div> -->
+
+                        <h4><a href="https://oke.io/6xca" target="_blank">Passive Income Blueprint with Amazon KDP</a></h4>
+                        <!-- <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, alias.</p> -->
+                    </div>
+                </div>
+            </div>
+
+            <div class="course-item cat2 col-lg-4 col-md-6">
+                <div class="course-block">
+                    <div class="course-img">
+                        <img src="assets/images/course/c4.png" alt="" class="img-fluid">
+                        <!-- <div class="course-price2">$210</div> -->
+                    </div>
+
+                    <div class="course-content">
+                        <!-- <div class="course-meta">
+                            <span class="course-student"><i class="fa fa-user-alt"></i>340 Students</span>
+                            <span class="course-duration"><i class="far fa-file-alt"></i>82 Lessons</span>
+                        </div> -->
+
+                        <h4><a href="https://oke.io/BmHz4u7">Data Analysis using SQL</a></h4>
+                        <!-- <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, alias.</p> -->
+                    </div>
+                </div>
+            </div>
+
+            <div class="course-item cat5 col-lg-4 col-md-6">
+                <div class="course-block">
+                    <div class="course-img">
+                        <img src="assets/images/course/c3.png" alt="" class="img-fluid">
+                        <!-- <div class="course-price2">$210</div> -->
+                    </div>
+
+                    <div class="course-content">
+                        <!-- <div class="course-meta">
+                            <span class="course-student"><i class="fa fa-user-alt"></i>340 Students</span>
+                            <span class="course-duration"><i class="far fa-file-alt"></i>82 Lessons</span>
+                        </div> -->
+
+                        <h4><a href="https://oke.io/s2h2VOBE" target="_blank">Mini Importation Goldmine Masterclass</a></h4>
+                        <!-- <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, alias.</p> -->
+                    </div>
+                </div>
+            </div>
+
+            <div class="course-item cat4 col-lg-4 col-md-6">
+                <div class="course-block">
+                    <div class="course-img">
+                        <img src="assets/images/course/c5.png" alt="" class="img-fluid">
+                        <!-- <div class="course-price2">$210</div> -->
+                    </div>
+
+                    <div class="course-content">
+                        <!-- <div class="course-meta">
+                            <span class="course-student"><i class="fa fa-user-alt"></i>340 Students</span>
+                            <span class="course-duration"><i class="far fa-file-alt"></i>82 Lessons</span>
+                        </div> -->
+
+                        <h4><a href="https://oke.io/Pux6O5tp" target="_blank">Digit101: The Ultimate Money Machine</a></h4>
+                        <!-- <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, alias.</p> -->
                     </div>
                 </div>
             </div>
